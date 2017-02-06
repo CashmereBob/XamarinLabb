73c2520fa339cd16a086c6d1b23427f750185421
Roppin.L <robin.ljungqvist@gmail.com>
2017-02-06 13:58:50 +0100

    Added schtuff for testing purposes

---
+++ TestPCL/TestPCL/TestPCL/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPCL
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public string Title { get; set; }
+
+        public Person()
+        {
+
+        }
+        public Person(string name, int age, string title)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Title = title;
+        }
+    }
+}
---
+++ TestPCL/TestPCL/TestPCL/PersonRepo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace TestPCL
+{
+    public class PersonRepo
+    {
+        public List<Person> Persons { get; set; }
+        public PersonRepo()
+        {
+            Persons = new List<Person>();
+            Persons.Add(new TestPCL.Person("Magnus", 30, "Praktikant"));
+            Persons.Add(new TestPCL.Person("Robin", 26, "Praktikant"));
+            Persons.Add(new TestPCL.Person("Anders", 50, "Scrum Master"));
+
+        }
+        public List<Person> getPersons()
+        {
+            return Persons;
+        }
+        public Person GetByName(string name)
+        {
+            var person = Persons.FirstOrDefault(x => x.Name == name);
+
+            return person;
+        }
+    }
+}
+
